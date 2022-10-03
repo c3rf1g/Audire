00cc693cd0dec355e0893bb18be6056ed5157808
c3rf1g <c3rf1g@gmail.com>
2022-10-03 15:57:35 +0300

    merged

---
+++ Audire/Helpers/Resources.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+enum Resources {
+    enum Colors {
+        static var inactive = UIColor(hexString: "#FF06FE")
+        static var tabBarBackground = UIColor(hexString: "#130E22")
+        static var backgroundColor = UIColor(hexString: "#1C1344")
+        static var borderHeadphones = UIColor(hexString: "#FA00FF")
+        static var topBorder = UIColor(hexString: "#EB00FF")
+    }
+}
+
+
